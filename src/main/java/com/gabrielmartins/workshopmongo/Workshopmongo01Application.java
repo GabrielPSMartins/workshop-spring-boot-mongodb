a02f8a73a40b78f224031c03827af01633d8c5a0
GabrielPSMartins <martins80gabriel@gmail.com>
2025-01-26 19:05:39 -0300

    Criando o projeto

---
+++ src/main/java/com/gabrielmartins/workshopmongo/Workshopmongo01Application.java
@@ -0,0 +1,13 @@
+package com.gabrielmartins.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshopmongo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workshopmongo01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielmartins/workshopmongo/Workshopmongo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielmartins.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshopmongo01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
